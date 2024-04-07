65dc14738e0cbaf3c54a7fd772407ff92075a1d0
Gary Bagnall <gary.bagnall@flightcentre.com>
2024-04-07 11:54:29 +1000

    link bookings

--- app/api/restaurant/[slug]/reserve/route.ts
+++ app/api/restaurant/[slug]/reserve/route.ts
@@ -1,3 +1,4 @@
+import { findAvailableTables } from "@/services/restaurant/findAvailableTables";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
@@ -15,11 +16,16 @@ export async function GET(
 
   const restaurant = await prisma.restaurant.findUnique({
     where: {
-      slug,
+      slug: params.slug,
+    },
+    select: {
+      tables: true,
+      open_time: true,
+      close_time: true,
     },
   });
 
-  if (!restaurant) {
+  if (!restaurant || !day || !time || !partySize) {
     return new Response(
       JSON.stringify({ errorMessage: "Invalid data provided" }),
       {
@@ -36,7 +42,73 @@ export async function GET(
       status: 400,
     });
   }
-  return NextResponse.json({ day, time, partySize, params });
+
+  const searchTimesWithTables = await findAvailableTables({
+    slug: params.slug,
+    time,
+    day,
+    restaurant,
+  });
+
+  if (searchTimesWithTables instanceof Response) {
+    return searchTimesWithTables;
+  }
+
+  const searchTimeWithTables = searchTimesWithTables.find((t) => {
+    return t.date.toISOString() === new Date(`${day}T${time}`).toISOString();
+  });
+
+  if (!searchTimeWithTables) {
+    return new Response(
+      JSON.stringify({ errorMessage: "Cannot book, no availability" }),
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const tablesCount: { 2: number[]; 4: number[] } = {
+    2: [],
+    4: [],
+  };
+
+  const tablesToBooks: number[] = [];
+  let seatsRemaining = parseInt(partySize);
+
+  while (seatsRemaining > 0) {
+    if (seatsRemaining >= 3) {
+      if (tablesCount[4].length) {
+        tablesToBooks.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      } else {
+        tablesToBooks.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      }
+    } else {
+      if (tablesCount[2].length) {
+        tablesToBooks.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      } else {
+        tablesToBooks.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      }
+    }
+  }
+
+  searchTimeWithTables.tables.forEach((table) => {
+    if (table.seats === 2) {
+      tablesCount[2].push(table.id);
+    }
+    if (table.seats === 4) {
+      tablesCount[4].push(table.id);
+    }
+  });
+
+  return NextResponse.json({ tablesToBooks });
 }
 
 //http://localhost:8069/api/restaurant/vivaan-fine-indian-cuisine-ottawa/reserve?day=2023-02-03&time=16:00:00.000Z&partySize=4
