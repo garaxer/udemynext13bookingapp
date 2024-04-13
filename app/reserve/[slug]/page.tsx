9620f0c1feff3a1ab694013e354922dbb6900891
Gary Bagnall <gary.bagnall@flightcentre.com>
2024-04-13 12:41:36 +1000

    add date-fns

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,17 +1,47 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
 import { Metadata } from "next";
+import { PrismaClient } from "@prisma/client";
+import { notFound } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Reserve",
   description: "Reserve a trailer",
 };
 
-function Reserve() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (!restaurant) {
+    notFound();
+  }
+
+  return restaurant;
+};
+
+async function Reserve({
+  params: { slug },
+  searchParams: { date, partySize },
+}: {
+  params: { slug: string };
+  searchParams: { date: string; partySize: string };
+}) {
+  const restaurant = await fetchRestaurantBySlug(slug);
   return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
-        <Header />
+        <Header
+          image={restaurant.main_image}
+          name={restaurant.name}
+          date={date}
+          partySize={partySize}
+        />
         <Form />
       </div>
     </div>
